86e8a2babfcd76b46540ee44946d7860a0a42c75
ashiq-rahman07 <web3.0.ashiq@gmail.com>
2024-08-20 12:39:25 +0600

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { Dashboard } from './pages/Dashboard';
+import { Auth } from './pages/Auth';
+
+function App() {
+  return (
+    <Router>
+      <div className='app-container'>
+          <Routes>
+            <Route path='/' element={<Dashboard/>} />
+            <Route path='/auth' element={<Auth/>} />
+          </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+   
+  </StrictMode>,
+)
